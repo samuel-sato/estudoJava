5349fc894ce3f7d4564fc732e81946343843087b
Samuel Sato <samuelsato2009@gmail.com>
2021-10-08 23:22:07 -0300

    doc

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"modelo","l":"Venda"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"view","l":"TelaVenda"}]
